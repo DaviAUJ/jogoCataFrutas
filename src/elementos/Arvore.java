3440c5267a406f652b9a4a148b10e30ecca223bb
William Gabriel Braga <70717232+Yckson@users.noreply.github.com>
2024-10-10 22:55:03 -0300

    Update Arvore.java
    Removendo o construtor duplicado após o merge do Javadoc

--- src/elementos/Arvore.java
+++ src/elementos/Arvore.java
@@ -19,18 +19,6 @@ public class Arvore extends ElementoEstatico {
     }
 
     /**
-     * Construtor da classe Arvore que define o nome e a posição da árvore.
-     *
-     * @param nome O nome, que será de qual fruta será a árvore.
-     * @param posX A coordenada x da posição da árvore.
-     * @param posY A coordenada y da posição da árvore.
-     */
-
-    public Arvore(String nome, int posX, int posY) {
-        super(nome, posX, posY);
-    }
-
-    /**
      * Construtor da classe Arvore que define o nome, posição e tipo da árvore.
      *
      * @param nome O nome da árvore.
